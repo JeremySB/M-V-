5a028fba2339c6f77d5c202e0654fc242b7a2dda
Jonathan Higgs <HiggsJI1@gcc.edu>
2017-02-12 13:32:40 -0500

    Fixed warning

--- Source/M(V)/Assets/Scripts/MouseLook.cs
+++ Source/M(V)/Assets/Scripts/MouseLook.cs
@@ -33,3 +33,35 @@ public class MouseLook : MonoBehaviour {
         transform.Rotate(deltaX, deltaY, deltaZ);
     }
 }
+
+public class MouseLook : MonoBehaviour {
+
+    public float sensivityVertical = 5.0f;
+    public float sensivityHorizontal = 5.0f;
+
+    public float minimumVertical = -45.0f;
+    public float maximumVertical = 45.0f;
+
+    private float rotationX = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //rotationX -= Input.GetAxis("Mouse Y") * sensivityVertical;
+        //rotationX = Mathf.Clamp(rotationX, minimumVertical, maximumVertical);
+        float deltaX = -Input.GetAxis("Mouse Y") * sensivityVertical;
+        rotationX = transform.localEulerAngles.x + deltaX;
+
+        // uses left and right arrows
+        float deltaY = Input.GetAxis("Horizontal") * sensivityHorizontal;
+
+        float deltaZ = -Input.GetAxis("Mouse X") * sensivityHorizontal;
+        //float rotationY = transform.localEulerAngles.y + deltaY;
+        //transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        transform.Rotate(deltaX, deltaY, deltaZ);
+    }
+}
