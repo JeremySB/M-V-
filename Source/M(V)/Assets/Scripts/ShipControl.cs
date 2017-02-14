7799d63b65a5ee2b2337e1a7bb1da1765fd3478b
Jeremy Bost <bostjs1@gcc.edu>
2017-02-13 20:39:54 -0500

    Some fun rigid body stuff

--- Source/M(V)/Assets/Scripts/ShipControl.cs
+++ Source/M(V)/Assets/Scripts/ShipControl.cs
@@ -14,15 +14,24 @@ public class ShipControl : MonoBehaviour {
     public float sensitivityYaw = 4.0f;
     public float sensitivityRoll = 3.0f;
 
-    CharacterController characterController;
+    public float thrusterPower = 1.5f;
+    public float brakeMultiplier = .95f;
+
+    Rigidbody rb;
+    
 
 	// Use this for initialization
 	void Start () {
-        
+        rb = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
+
+        /*
+         * Rotations
+         */
+
         // pitch
         float deltaX = Input.GetAxis("Pitch") * sensitivityPitch;
 
@@ -33,5 +42,22 @@ public class ShipControl : MonoBehaviour {
         float deltaZ = Input.GetAxis("Roll") * sensitivityRoll;
 
         transform.Rotate(deltaX, deltaY, deltaZ);
-	}
+
+    }
+
+    // FixedUpdate is better for forces/RB stuff
+    void FixedUpdate() { 
+
+        /*
+         * Thrust/forces
+         */
+
+        float thrust = Input.GetAxis("Thrust") * thrusterPower;
+        rb.AddForce(thrust * transform.forward);
+
+        if (Input.GetAxis("Brake") != 0)
+            rb.velocity *= brakeMultiplier;
+
+        
+    }
 }
