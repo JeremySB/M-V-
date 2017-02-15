e758253504b5ece73599f7a1b47d86b8ce062051
Noah Newell <newellnd1@gcc.edu>
2017-02-15 04:03:33 -0500

    Additions to level, textures not loading, needs polishing, basics done

---
+++ Source/M(V)/Assets/Scripts/ObstacleSpin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpin : MonoBehaviour {
+    private int spinSpeed;
+	// Use this for initialization
+	void Start () {
+        spinSpeed = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(spinSpeed * Time.deltaTime, 0, 0);
+    }
+}
