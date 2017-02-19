5d4102a468b5efe07773c94f47da4153626941ab
Jeremy Bost <bostjs1@gcc.edu>
2017-02-19 17:54:38 -0500

    Thruster particles

---
+++ Source/M(V)/Assets/Scripts/ThrusterParticlesControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrusterParticlesControl : MonoBehaviour {
+
+    private ParticleSystem particles;
+
+	// Use this for initialization
+	void Start () {
+        particles = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetAxis("Thrust") > 0.01)
+        {
+            particles.Play();
+        }
+        else
+        {
+            particles.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+        
+	}
+}
