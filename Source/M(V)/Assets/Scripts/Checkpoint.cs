7c3ae0f65a278281fc6c6e522a256b16f6497d37
Unknown <biomyth1@gmail.com>
2017-02-26 23:49:31 -0500

    Array based checkpoints

---
+++ Source/M(V)/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+	Checkpoints parent;
+	// Use this for initialization
+	void Start () {
+		parent = this.transform.parent.GetComponent<Checkpoints>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && GetComponent<Renderer>().enabled)
+        {
+            GetComponent<AudioSource>().Play();
+			parent.next ();
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Source/M(V)/Assets/Scripts/Checkpoints.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoints : MonoBehaviour {
+	public Checkpoint[] checkpoints;
+	public Checkpoint currentCheckpoint;
+	private int currentCheckpointIndex = 0;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < checkpoints.Length; i++)
+			checkpoints [i].GetComponent<Renderer> ().enabled = false;
+		
+		currentCheckpoint = checkpoints [currentCheckpointIndex];
+		currentCheckpoint.GetComponent<Renderer> ().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void next(){
+		currentCheckpoint.GetComponent<Renderer> ().enabled = false;
+		if(currentCheckpointIndex < checkpoints.Length){
+			currentCheckpoint = checkpoints [++currentCheckpointIndex];
+			currentCheckpoint.GetComponent<Renderer> ().enabled = true;
+		}
+	}
+}
