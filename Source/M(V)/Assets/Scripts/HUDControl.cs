cb111ad9dfd2eade1817a9d39a84ec29ee6aa466
Jeremy Bost <bostjs1@gcc.edu>
2017-02-26 22:11:30 -0500

    Started on timer text, moved HUD to under player

--- Source/M(V)/Assets/Scripts/HUDControl.cs
+++ Source/M(V)/Assets/Scripts/HUDControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDControl : MonoBehaviour {
 
@@ -8,6 +9,9 @@ public class HUDControl : MonoBehaviour {
     public Camera cam;
     public RectTransform dot;
     public RectTransform circle;
+    public Text timerText; 
+
+    private float timer = -1;
 
     // Use this for initialization
     void Start () {
@@ -16,6 +20,15 @@ public class HUDControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if(timer >= 0)
+        {
+            timer += Time.deltaTime;
+        }
+        else if(ship.velocity.magnitude > 1)
+        {
+            timer = 0;
+        }
+
         circle.anchoredPosition3D = cam.WorldToScreenPoint(ship.transform.forward + cam.transform.position);
         dot.anchoredPosition3D = cam.WorldToScreenPoint(ship.velocity + cam.transform.position) + new Vector3(0, 0, 0);
 
@@ -29,4 +42,19 @@ public class HUDControl : MonoBehaviour {
         }
         
 	}
+
+    private void OnGUI()
+    {
+        if (timer >= 0)
+        {
+            int minutes = Mathf.FloorToInt(timer / 60F);
+            int seconds = Mathf.FloorToInt(timer - minutes * 60);
+            string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+            timerText.text = "Time Elapsed:\n" + niceTime;
+        }
+        else
+        {
+            timerText.text = "Start moving to begin!";
+        }
+    }
 }
