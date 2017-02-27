4ed94c143100524d7fd22e11d2a3ef46a87bade8
Jonathan Higgs <HiggsJI1@gcc.edu>
2017-02-26 22:10:50 -0500

    Audio and astroids

---
+++ Source/M(V)/Assets/Scripts/AsteroidSpin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpin : MonoBehaviour {
+    public float spd;
+    public Vector3 rotation;
+	// Use this for initialization
+	void Start () {
+        //spd = Random.Range(0.02f, 0.3f);
+        spd = 1 - (transform.localScale.magnitude / 50000f);
+        rotation = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
+        rotation.x *= spd;
+        rotation.y *= spd;
+        rotation.z *= spd;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotation);
+	}
+}
