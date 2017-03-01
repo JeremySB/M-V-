c72ed7444dc93e9eff249cf4c108128e7b56d2c8
Jonathan Higgs <HiggsJI1@gcc.edu>
2017-02-28 23:45:28 -0500

    Fixed inter-asteroid collision errors
    Normalized asteroid spin, added asteroids

--- Source/M(V)/Assets/Scripts/AsteroidSpin.cs
+++ Source/M(V)/Assets/Scripts/AsteroidSpin.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class AsteroidSpin : MonoBehaviour {
     public float spd;
     public Vector3 rotation;
+
 	// Use this for initialization
 	void Start () {
         spd = 0.05f;
-        //spd = 1 - (transform.localScale.magnitude / 50000f);
-        rotation = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1));
+        rotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
         rotation *= spd;
     }
 	
