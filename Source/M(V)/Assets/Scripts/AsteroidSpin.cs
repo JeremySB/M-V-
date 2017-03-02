89cd9af3ed30696e96b9af9920cb71b203bf39c9
Jonathan Higgs <HiggsJI1@gcc.edu>
2017-03-01 23:25:00 -0500

    Fixed asteroid spin

--- Source/M(V)/Assets/Scripts/AsteroidSpin.cs
+++ Source/M(V)/Assets/Scripts/AsteroidSpin.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AsteroidSpin : MonoBehaviour {
-    public float spd;
+    public float spd = 0.07f;
     public Vector3 rotation;
 
 	// Use this for initialization
 	void Start () {
-        spd = 0.05f;
-        rotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        rotation = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
         rotation *= spd;
     }
 	
