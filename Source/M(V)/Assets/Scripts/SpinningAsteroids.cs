42ff36d14f5d8532704c4c516f4b67feea95bccb
Jonathan Higgs <HiggsJI1@gcc.edu>
2017-03-01 19:43:04 -0500

    Final Asteroids obstacles
    First one working

---
+++ Source/M(V)/Assets/Scripts/SpinningAsteroids.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningAsteroids : MonoBehaviour {
+    public float spd;
+
+
+	// Use this for initialization
+	void Start () {
+        spd = 0.6f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0,0,spd,Space.Self);
+	}
+}
